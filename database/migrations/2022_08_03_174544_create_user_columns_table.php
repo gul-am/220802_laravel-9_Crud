fc9403269e3a62871996de72a03e250d5b2a1668
gul-amd-moz <100712631+gul-am@users.noreply.github.com>
2022-08-03 21:39:44 +0200

    build: db

--- database/migrations/2022_08_03_174544_create_user_columns_table.php
+++ database/migrations/2022_08_03_174544_create_user_columns_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('contact');
-            $table->string('sector');
+            $table->string('sector', ['','Warehouse','Production','Freezers','Maintenance',])->default('Warehouse');
             $table->enum('gender', ['','M','F'])->default('M');
             $table->timestamps();
         });
