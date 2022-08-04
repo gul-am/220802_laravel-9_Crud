12b3a36b447e87312e8c21fad5cc4b1ac8a30183
gul-amd-moz <100712631+gul-am@users.noreply.github.com>
2022-08-04 02:17:32 +0200

    chore: final changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,12 +7,18 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function index(){
+
+        $users = UserColumn::all();
+        return view ('welcome', compact('users'));
+    }
+
     public function create(){
         return view ('create');
     }
 
-    public function edit(){
-        return view ('edit');
+    public function edit(UserColumn $user){
+        return view ('edit', compact('user'));
     }
 
     public function store(Request $request){
@@ -23,12 +29,34 @@ class UserController extends Controller
                 'gender' => 'required|min:1|max:1'
             ]);
 
-            UserColumn::create($request->all);
+            UserColumn::create($request->all());
 
             return redirect()->back()->with('success', 'Data saved');
     }
 
-    public function destroy(){
+    public function delete(UserColumn $user){
+
+        $user->delete();
+        return redirect()->route('home')->with('sucess', 'User deleted !');
+
+    }
+
+    public function update(UserColumn $user, Request $request){
+
+        $request -> validate([
+            'name' => 'required',
+            'contact' => 'required|integer',
+            'sector' => 'required|min:8|max:12',
+            'gender' => 'required|min:1|max:1'
+        ]);
+
+        $user->name = $request->name;
+        $user->contact = $request->contact;
+        $user->sector = $request->sector;
+        $user->gender = $request->gender;
+
+        $user->save();
+        return redirect()->route('home')->with('sucess', 'User updated !');
 
     }
 }
